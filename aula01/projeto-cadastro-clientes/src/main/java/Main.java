310a34c3e1bfdd29cfa3120d31994bd5b6c6c89e
Karmaicom <karmaicom@gmail.com>
2026-05-11 12:55:19 -0300

    Criacao do metodo salvarDados()

--- aula01/projeto-cadastro-clientes/src/main/java/Main.java
+++ aula01/projeto-cadastro-clientes/src/main/java/Main.java
@@ -1,5 +1,11 @@
 package main.java;
 
+import main.java.entites.Cliente;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.UUID;
+
 public class Main {
 
     static void main(String[] args) {
@@ -7,7 +13,29 @@ public class Main {
         System.out.println("SISTEMA DE CADASTRO DE CLIENTES");
         System.out.println("Preencha os dados do cliente: ");
 
-        
+        // Criando um objeto para usarmos a classe Scanner do Java
+        var scanner = new Scanner(System.in);
+
+        // Criando um objeto para acessar o conteudo da classe cliente
+        var cliente = new Cliente();
+
+        cliente.id = UUID.randomUUID(); // gerando o ID do cliente
+        cliente.dataHoraCadastro = LocalDateTime.now(); // gerando a data e hora atual
+
+        System.out.print("\nInforme o nome do cliente.........: ");
+        cliente.nome = scanner.nextLine();
+
+        System.out.print("\nInforme o email...................: ");
+        cliente.email = scanner.nextLine();
+
+        System.out.print("\nInforme o telefone................: ");
+        cliente.telefone = scanner.nextLine();
+
+        System.out.print("\nInforme o cpf.....................: ");
+        cliente.cpf = scanner.nextLine();
+
+        //
+        cliente.salvarDados();
 
     }
 
--- aula01/projeto-cadastro-clientes/src/main/java/entites/Cliente.java
+++ aula01/projeto-cadastro-clientes/src/main/java/entites/Cliente.java
@@ -1,5 +1,7 @@
 package main.java.entites;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -14,7 +16,39 @@ public class Cliente {
     public String email;
     public String telefone;
     public String cpf;
-    public LocalDateTime dataHoraCadastor;
+    public LocalDateTime dataHoraCadastro;
+
+    // Metodo para salvar os dados do cliente em um arquivo
+    public void salvarDados() {
+        try {
+
+            // Definindo o local e o nome do arquivo
+            var fileWriter =
+                    new FileWriter(
+                            "c:\\temp",
+                            true);
+
+            // Escrever no arquivo
+            var printWriter = new PrintWriter(fileWriter);
+
+            printWriter.println(id);
+            printWriter.println(nome);
+            printWriter.println(email);
+            printWriter.println(telefone);
+            printWriter.println(cpf);
+            printWriter.println(dataHoraCadastro);
+            printWriter.println("*************************");
+
+            // Fechar o arquivo
+            fileWriter.close();
+            printWriter.close();
+
+            System.out.println("\nDados gravados com sucesso!");
+
+        } catch (Exception e) {
+            System.out.println("Falha ao gravar o arquivo: " + e.getMessage());
+        }
+    }
 
 
 }
