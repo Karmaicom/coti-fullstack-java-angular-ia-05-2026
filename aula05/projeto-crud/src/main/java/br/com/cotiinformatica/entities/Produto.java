efd69d078eaec8beab39afc2d7a1ff360fc7c812
Karmaicom Rocha <karmaicom@gmail.com>
2026-05-20 11:17:42 -0300

    Criacao da classe de entidade Produto.

---
+++ aula05/projeto-crud/src/main/java/br/com/cotiinformatica/entities/Produto.java
@@ -0,0 +1,60 @@
+package br.com.cotiinformatica.entities;
+
+public class Produto {
+
+    private Integer id;
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+
+    public Produto() { }
+
+    public Produto(Integer id, String nome, Double preco, Integer quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
