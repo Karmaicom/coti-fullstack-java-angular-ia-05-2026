775449afdf3645a3b69c2599fb028e872d14badf
Karmaicom Rocha <karmaicom@gmail.com>
2026-05-20 12:17:31 -0300

    Criação da classe que representa uma fábrica de conexões com o banco de dados MySql.

---
+++ aula05/projeto-crud/src/main/java/br/com/cotiinformatica/factories/ConnectionFactory.java
@@ -0,0 +1,34 @@
+package br.com.cotiinformatica.factories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+    private static final String host = """
+                jdbc:mysql://localhost:3307/bd_produtos
+                ?useSSL=false
+                ?serverTimezone=America/Sao_Paulo
+                ?allowPublicKeyRetrieval=true
+            """;
+    private static final String user = "coti";
+    private static final String password = "coti";
+
+    /**
+     * Devolve uma conexao aberta com o banco de dados
+     * @return
+     * @throws Exception
+     */
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(host, user, password);
+    }
+
+    /**
+     * Fechar a conexao com o banco de dados
+     * @param connection
+     * @throws Exception
+     */
+    public static void closeConnection(Connection connection) throws Exception {
+        connection.close();
+    }
+}
