3a0d2a1e7ef0faef081db02a4a9ecd82446fcc90
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-01 10:57:19 -0300

    Implementado, no controller, o método para inserção de um produto no banco de dados.

--- aula08/api-produtos/src/main/java/br/com/cotiinformatica/apiprodutos/controllers/ProdutoController.java
+++ aula08/api-produtos/src/main/java/br/com/cotiinformatica/apiprodutos/controllers/ProdutoController.java
@@ -1,10 +1,22 @@
 package br.com.cotiinformatica.apiprodutos.controllers;
 
+import br.com.cotiinformatica.apiprodutos.dtos.ProdutoRequest;
+import br.com.cotiinformatica.apiprodutos.entities.Produto;
+import br.com.cotiinformatica.apiprodutos.intefaces.IProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.SpringServletContainerInitializer;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/produtos")
 public class ProdutoController {
+
+    private IProdutoRepository repository;
+
+    @Autowired
+    public ProdutoController(IProdutoRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/listar")
     public String listar(){
@@ -12,8 +24,23 @@ public class ProdutoController {
     }
 
     @PostMapping("/criar")
-    public String criar() {
-        return "Produto criado com sucesso!";
+    public String criar(@RequestBody ProdutoRequest produtoRequest) {
+        try {
+            var produto = new Produto();
+            produto.setNome(produtoRequest.nome());
+            produto.setDescricao(produtoRequest.descricao());
+            produto.setPreco(produtoRequest.preco());
+            produto.setQuantidade(produtoRequest.quantidade());
+
+            repository.inserirProduto(produto);
+
+            return "Produto criado com sucesso!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ERROR: " + e.getMessage());
+            return "Erro ao tentar inserir produto!";
+        }
+
     }
 
     @PutMapping("/alterar")
