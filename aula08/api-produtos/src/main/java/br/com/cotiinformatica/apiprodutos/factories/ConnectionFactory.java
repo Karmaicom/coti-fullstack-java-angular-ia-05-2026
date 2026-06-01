23e3511a0182409f7a9412e4e167b7f189de85a1
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-01 10:56:14 -0300

    Incluído o tratamento de exceção.

--- aula08/api-produtos/src/main/java/br/com/cotiinformatica/apiprodutos/factories/ConnectionFactory.java
+++ aula08/api-produtos/src/main/java/br/com/cotiinformatica/apiprodutos/factories/ConnectionFactory.java
@@ -3,14 +3,19 @@ package br.com.cotiinformatica.apiprodutos.factories;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ConectionFactory {
+public class ConnectionFactory {
 
     private static String url = "jdbc:prostgres://localhost:5433/bd-api-produtos";
     private static String usuario = "coti";
     private static String senha = "COti@2026";
 
-    public static Connection getConnection() throws Exception {
-        return DriverManager.getConnection(url, usuario, senha);
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
 
 }
