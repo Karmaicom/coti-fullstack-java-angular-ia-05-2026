ec27b3ca5949ae4088595d5fa92ee4e430fb70bf
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-01 12:29:51 -0300

    Correção na url que monta a string de conexão.

--- aula08/api-produtos/src/main/java/br/com/cotiinformatica/apiprodutos/factories/ConnectionFactory.java
+++ aula08/api-produtos/src/main/java/br/com/cotiinformatica/apiprodutos/factories/ConnectionFactory.java
@@ -5,13 +5,14 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
 
-    private static String url = "jdbc:prostgres://localhost:5433/bd-api-produtos";
+    private static String url = "jdbc:postgresql://localhost:5433/bd-api-produtos";
     private static String usuario = "coti";
-    private static String senha = "COti@2026";
+    private static String senha = "Coti@2026";
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(url, usuario, senha);
+            var connection = DriverManager.getConnection(url, usuario, senha);
+            return connection;
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("ERROR: " + e.getMessage());
