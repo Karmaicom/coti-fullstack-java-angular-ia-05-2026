c51d6605e6d73c06debee7747d48cc9451622058
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-09 17:09:32 -0300

    Scripts de análise após debug dos endpoints.

--- Lista de Exercicios/Lista 01/api-clientes/src/main/java/br/com/cotiinformatica/apiclientes/scripts/schema.sql
+++ Lista de Exercicios/Lista 01/api-clientes/src/main/java/br/com/cotiinformatica/apiclientes/scripts/schema.sql
@@ -1,12 +1,35 @@
 
 create table cliente (
-    id                  UUID              primary key not null,
-    nome                varchar(100)        not null,
-    email               varchar(100)        not null unique,
-    cpf                 char(14)            not null unique,
-    telefone            varchar(20)         not null,
-    data_cadastro       timestamp           not null    default current_timestamp, -- Já cadastra com data atual
-    data_atualizacao    timestamp           null,
-    data_exclusao       timestamp           null,
-    ativo               int                 not null    default 1 -- Cliente é cadastrado automaticamente como ativo
+     id                  uuid              primary key not null,
+     nome                varchar(100)        not null,
+     email               varchar(100)        not null unique,
+     cpf                 char(14)            not null unique,
+     telefone            varchar(20)         not null,
+     data_cadastro       timestamp           not null    default current_timestamp, -- Já cadastra com data atual
+     data_atualizacao    timestamp           null,
+     data_exclusao       timestamp           null,
+     ativo               int                 not null    default 1 -- Cliente é cadastrado automaticamente como ativo
 );
+
+select * from cliente;
+
+
+select id, nome, email, cpf, telefone,
+       data_cadastro, data_atualizacao, data_exclusao, ativo
+from cliente where ativo = 1 and id = ('a2c3be30-0594-4fdc-aa86-8801567f9258'::uuid)
+
+
+select id, nome, email, cpf, telefone,
+       data_cadastro, data_atualizacao, data_exclusao, ativo
+from cliente
+where ativo = 1;
+
+
+
+update cliente set
+                   nome = ('Fulano da Silva Machado') ,
+                   email = ('fulanodasilvamachado@gmail.com'),
+                   cpf = ('111.111.111-11'),
+                   telefone = ('(21) 99111-1111'),
+                   data_atualizacao = ('2026-06-09 16:25:04.658776-03')
+where ativo = 1 and id = ('a2c3be30-0594-4fdc-aa86-8801567f9258'::uuid)
