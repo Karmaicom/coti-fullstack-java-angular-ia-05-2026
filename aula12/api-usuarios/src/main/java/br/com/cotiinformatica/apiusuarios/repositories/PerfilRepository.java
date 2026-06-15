be1dbcdfd74959ecf6ae9234a6f74753fb7588eb
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-15 12:58:59 -0300

    Criação das classe respository de Perfil e Usuario.

---
+++ aula12/api-usuarios/src/main/java/br/com/cotiinformatica/apiusuarios/repositories/PerfilRepository.java
@@ -0,0 +1,85 @@
+package br.com.cotiinformatica.apiusuarios.repositories;
+
+import br.com.cotiinformatica.apiusuarios.entities.Perfil;
+import br.com.cotiinformatica.apiusuarios.factories.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PerfilRepository {
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
+    public void inserir(Perfil perfil) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    insert into perfis (nome) values (?)
+                """);
+            statement.setString(1, perfil.getNome());
+            statement.execute();
+        }
+    }
+
+    public boolean atualizar(Perfil perfil) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    update perfis set nome = ? where id = ?
+                """);
+            statement.setString(1, perfil.getNome());
+            statement.setInt(2, perfil.getId());
+            return statement.executeUpdate() > 0;
+        }
+    }
+
+    public boolean excluir(Perfil perfil) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    delete from perfis where id = ?
+                """);
+            statement.setInt(1, perfil.getId());
+            return statement.executeUpdate() > 0;
+        }
+    }
+
+    public Perfil buscarPorId(Integer id) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    select id, nome from perfis where id = ?
+                """);
+            statement.setInt(1, id);
+            var resultSet = statement.executeQuery();
+
+            Perfil perfilFound = null;
+            while(resultSet.next()) {
+                perfilFound = new Perfil();
+                perfilFound.setId(resultSet.getInt("id"));
+                perfilFound.setNome(resultSet.getString("nome"));
+            };
+
+            return perfilFound;
+        }
+    }
+
+    public List<Perfil> listar() throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    select id, nome from perfis order by nome;
+                """);
+
+            var lista = new ArrayList<Perfil>();
+            var resultSet = statement.executeQuery();
+            while(resultSet.next()) {
+                var perfilFound = new Perfil();
+                perfilFound.setId(resultSet.getInt("id"));
+                perfilFound.setNome(resultSet.getString("nome"));
+                lista.add(perfilFound);
+            };
+
+            return lista;
+        }
+    }
+}
---
+++ aula12/api-usuarios/src/main/java/br/com/cotiinformatica/apiusuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,108 @@
+package br.com.cotiinformatica.apiusuarios.repositories;
+
+import br.com.cotiinformatica.apiusuarios.entities.Usuario;
+import br.com.cotiinformatica.apiusuarios.factories.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
+    public void inserir(Usuario usuario) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    insert into 
+                        usuarios (nome, email, senha, data_hora_cadastro, perfil_id) 
+                        values (?, ?, ?, ?, ?);
+                """);
+            statement.setString(1, usuario.getNome());
+            statement.setString(2, usuario.getEmail());
+            statement.setString(3, usuario.getSenha());
+            statement.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            statement.setInt(5, usuario.getPerfilId());
+            statement.execute();
+        }
+    }
+
+    public boolean atualizar(Usuario usuario) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    update usuarios 
+                           set nome = ?, email = ?, senha = ?, perfil_id = ? 
+                           where id = ?
+                """);
+            statement.setString(1, usuario.getNome());
+            statement.setString(2, usuario.getEmail());
+            statement.setString(3, usuario.getSenha());
+            statement.setInt(4, usuario.getPerfilId());
+            statement.setInt(5, usuario.getId());
+            return statement.executeUpdate() > 0;
+        }
+    }
+
+    public boolean excluir(Integer id) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    delete from usuarios where id = ?
+                """);
+            statement.setInt(1, id);
+            return statement.executeUpdate() > 0;
+        }
+    }
+
+    public Usuario buscarPorId(Integer id) throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    select id, nome, email, senha, data_hora_cadastro, perfil_id 
+                        from perfis where id = ?
+                """);
+            statement.setInt(1, id);
+            var resultSet = statement.executeQuery();
+
+            Usuario usuarioFound = null;
+            while(resultSet.next()) {
+                usuarioFound = new Usuario();
+                usuarioFound.setId(resultSet.getInt("id"));
+                usuarioFound.setNome(resultSet.getString("nome"));
+                usuarioFound.setEmail(resultSet.getString("email"));
+                usuarioFound.setSenha(resultSet.getString("senha"));
+                usuarioFound.setDataHoraCadastro(LocalDate.from(resultSet.getTimestamp("data_hora_cadastro").toLocalDateTime()));
+                usuarioFound.setPerfilId(resultSet.getInt("perfil_id"));
+            };
+
+            return usuarioFound;
+        }
+    }
+
+    public List<Usuario> listar() throws Exception {
+        try (var connection = connectionFactory.getConnection()) {
+            var statement = connection.prepareStatement("""
+                    select id, nome from usuarios order by nome;
+                """);
+
+            var lista = new ArrayList<Usuario>();
+            var resultSet = statement.executeQuery();
+            while(resultSet.next()) {
+                var usuarioFound = new Usuario();
+                usuarioFound.setId(resultSet.getInt("id"));
+                usuarioFound.setNome(resultSet.getString("nome"));
+                usuarioFound.setEmail(resultSet.getString("email"));
+                usuarioFound.setSenha(resultSet.getString("senha"));
+                usuarioFound.setDataHoraCadastro(LocalDate.from(resultSet.getTimestamp("data_hora_cadastro").toLocalDateTime()));
+                usuarioFound.setPerfilId(resultSet.getInt("perfil_id"));
+            };
+
+            return lista;
+        }
+    }
+
+}
