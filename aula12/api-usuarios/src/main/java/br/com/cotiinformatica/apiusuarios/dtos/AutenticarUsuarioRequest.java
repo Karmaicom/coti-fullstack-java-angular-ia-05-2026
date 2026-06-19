8fe25824c4791a960ecb7eec9c455e063dc06402
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-19 13:19:20 -0300

    Ajustes no record AutenticarUsuarioRequest.

--- aula12/api-usuarios/src/main/java/br/com/cotiinformatica/apiusuarios/dtos/AutenticarUsuarioRequest.java
+++ aula12/api-usuarios/src/main/java/br/com/cotiinformatica/apiusuarios/dtos/AutenticarUsuarioRequest.java
@@ -3,12 +3,7 @@ package br.com.cotiinformatica.apiusuarios.dtos;
 import java.time.LocalDateTime;
 
 public record AutenticarUsuarioRequest(
-        Integer id,
-        String nome,
         String email,
-        String senha,
-        String perfil,
-        LocalDateTime dataHoraAcesso,
-        String accessToken
+        String senha
 ) {
 }
