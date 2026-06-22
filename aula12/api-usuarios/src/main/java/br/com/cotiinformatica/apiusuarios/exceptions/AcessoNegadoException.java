6de274a5690901b638bc7435b9171f3c199999f4
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-22 11:20:47 -0300

    Inclusao de comentario na classe de excessão AcessoNegado.

--- aula12/api-usuarios/src/main/java/br/com/cotiinformatica/apiusuarios/exceptions/AcessoNegadoException.java
+++ aula12/api-usuarios/src/main/java/br/com/cotiinformatica/apiusuarios/exceptions/AcessoNegadoException.java
@@ -2,6 +2,10 @@ package br.com.cotiinformatica.apiusuarios.exceptions;
 
 public class AcessoNegadoException extends RuntimeException {
 
+    /**
+     * Implementar um metodo para retornar a mensagem
+     * de erro padrao desta classe de excessão
+     */
     @Override
     public String getMessage() {
         return "Acesso negado.";
