c9ccf46800b358a05653cc821e3e45ee88caa63e
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-23 16:39:21 -0300

    Criação da ConnectionFactory

---
+++ aula12/api-usuarios-fix/src/main/java/br/com/cotiinformatica/apiusuariosfix/factories/ConnectionFactory.java
@@ -0,0 +1,24 @@
+package br.com.cotiinformatica.apiusuariosfix.factories;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@Component
+public class ConnectionFactory {
+
+    @Value("${datasource.url}")
+    private String url;
+
+    @Value("${datasource.username}")
+    private String username;
+
+    @Value("${datasource.password}")
+    private String password;
+
+    public Connection getConnection() throws Exception{
+        return DriverManager.getConnection(url, username, password);
+    }
+}
