9c788e755da2ec6d9e58c0f0c645a802ec3540ac
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-23 14:40:53 -0300

    Criação inicial do projeto + configuração docker-compose.

---
+++ aula12/api-usuarios-fix/src/main/java/br/com/cotiinformatica/apiusuariosfix/ApiUsuariosFixApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.apiusuariosfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsuariosFixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUsuariosFixApplication.class, args);
+    }
+
+}
---
+++ aula12/api-usuarios-fix/src/test/java/br/com/cotiinformatica/apiusuariosfix/ApiUsuariosFixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.apiusuariosfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosFixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
